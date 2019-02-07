00cd236f2be49a883de7ccd3384ede863f13a393
bobgeng <44568645+bobgeng@users.noreply.github.com>
2019-02-07 21:27:17 +0100

    new Board create, adjust yesterday's code

--- src/TicTacToeSample/Client.java
+++ src/TicTacToeSample/Client.java
@@ -1,4 +1,4 @@
-package TicTacToe;
+package TicTacToeSample;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
--- src/TicTacToeSample/Server.java
+++ src/TicTacToeSample/Server.java
@@ -1,4 +1,4 @@
-package TicTacToe;
+package TicTacToeSample;
 
 import java.io.BufferedReader;
 import java.io.IOException;
